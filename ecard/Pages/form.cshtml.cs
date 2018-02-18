b68f388b7ead1b6e71c3222dcdda23c4393c7908
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2018-02-17 17:27:12 -0800

    .

---
+++ ecard/Pages/form.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ecard.Pages
+{
+    public class FormModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+    }
+}
